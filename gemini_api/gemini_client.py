9108e6e01b44da4196f6a638ed50d98f8fac02cd
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2025-09-06 16:00:27 +0700

    update: add llmchain

--- gemini_api/gemini_client.py
+++ gemini_api/gemini_client.py
@@ -1,22 +1,67 @@
-from google import genai
 import os
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
 
 load_dotenv()
 
 class GeminiClient:
     def __init__(self):
-        self.client = genai.Client(
-            api_key=os.getenv("GEMINI_API_KEY"),
+        self.llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+        self.prompt_template = """
+        You are an expert software documentation writer.
+
+        I will provide you a codebase from GitHub repository {github_repo_url} as a dictionary, 
+        where each key is a file path and each value is the source code of that file.
+
+        Please write a complete, clear, and professional project documentation in Markdown format based on this codebase.
+
+        If the dictionary keys do NOT include a file named `requirements.txt`, 
+        please write the documentation **without** a Prerequisites section.
+
+        # Project Title
+
+        A short description of the project.
+
+        ## Features
+        - Key features of the project.
+
+        ## Prerequisites
+        - Required software and libraries.
+
+        ## Installation
+        Step-by-step instructions on how to install the project.
+
+        ## Usage
+        How to run and use the project.
+
+        ## Code Structure
+        Explanation of the main folders and important files.
+
+        Here is the codebase (file path : code content):
+
+        {code}
+
+        Please generate the full Markdown documentation now.
+
+        Use English language only
+        """
+
+        self.prompt = PromptTemplate(
+            template= self.prompt_template,
+            input_variables=["github_repo_url", "code"]
         )
 
-        self.model_name = os.getenv("GEMINI_MODEL_NAME")
-
-    def generate_text(self, prompt): 
-        response = self.client.models.generate_content(
-            model=self.model_name,
-            contents=prompt,
-            # max_tokens=int(os.getenv("GEMINI_MAX_TOKENS", 1024)),
-            # temperature=float(os.getenv("GEMINI_TEMPERATURE", 0.7))
+        self.chain = LLMChain(
+            llm=self.llm,
+            prompt=self.prompt,
+            verbose=True
         )
-        return response.text
+
+    def generate_text(self, github_repo_url, code): 
+        response = self.chain.invoke({
+            "github_repo_url": github_repo_url,
+            "code": code
+        })
+        return response["text"]
